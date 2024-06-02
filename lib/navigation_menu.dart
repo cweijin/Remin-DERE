62bbc456233341249a0eefb3c9fd3a5d0af8831c
Wei Jin <chongweijin13@gmail.com>
2024-06-03 05:44:27 +0800

    Logout Button
    Added a simple logout button under profile to aid demonstration.

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -6,6 +6,7 @@ import 'package:remindere/features/body/screens/chat/chat.dart';
 import 'package:remindere/features/body/screens/home/home.dart';
 import 'package:remindere/features/body/screens/notification/notification.dart';
 import 'package:remindere/features/body/screens/profile/profile.dart';
+import 'package:remindere/features/personalization/screens/settings/settings.dart';
 import 'package:remindere/utils/constants/colors.dart';
 import 'package:remindere/utils/helpers/helper_functions.dart';
 
@@ -75,6 +76,6 @@ class NavigationController extends GetxController {
     const CalendarScreen(),
     const ChatScreen(),
     const NotificationScreen(),
-    const ProfileScreen(),
+    const SettingScreen(),
   ];
 }
