1c6e614831a9ae7539872ec8f23a9826d365fc40
testing1234567891011121314 <90443963+testing1234567891011121314@users.noreply.github.com>
2024-07-01 01:14:09 +0800

    first test
    widget test for manual task allocation

---
+++ lib/test/manual_allocation_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:remindere/features/taskallocation/screens/manual_allocation/manual_allocation.dart';
+
+void main() {
+  testWidgets("Manual allocation test", (WidgetTester tester) async {
+    // Find all widgets needed
+    final datePicker = find.byElementType(TextField);
+    final createTask = find.byElementType(ElevatedButton);
+
+    // execute the actual test
+    await tester.pumpWidget(const MaterialApp(home: ManualAllocation()));
+    await tester.tap(datePicker);
+    await tester.tap(createTask);
+    await tester.pump;
+
+    // check outputs
+    expect(find.text("Make Widget Testing video"), findsAtLeastNWidgets(2));
+  });
+}
